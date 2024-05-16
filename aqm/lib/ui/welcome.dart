a72b33f7bc252e9fba38fbe6355084ca334d5342
sammyniyo <samshakul@gmail.com>
2024-05-16 13:06:48 +0200

    welcome page

--- aqm/lib/ui/welcome.dart
+++ aqm/lib/ui/welcome.dart
@@ -25,15 +25,66 @@ class _WelcomeState extends State<Welcome> {
         title: Text(selectedLocation.length.toString() + ' Selected'),
       ),
       body: ListView.builder(
-          itemCount: cities.length,
-          itemBuilder: (BuildContext context, int index) {
-            return Container(
-              margin: const EdgeInsets.only(left: 10, top: 10, right: 10),
-              padding: const EdgeInsets.symmetric(horizontal: 20),
-              height: size.height,
-              width: size.width,
-            );
-          }),
+        itemCount: cities.length,
+        itemBuilder: (BuildContext context, int index) {
+          return Container(
+            margin: const EdgeInsets.only(left: 10, top: 10, right: 10),
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            height: size.height * 0.08,
+            decoration: BoxDecoration(
+              border: cities[index].isSelected
+                  ? Border.all(
+                      color: myConstants.secondaryColor.withOpacity(.6),
+                      width: 2,
+                    )
+                  : Border.all(color: Colors.white),
+              borderRadius: const BorderRadius.all(Radius.circular(10)),
+              boxShadow: [
+                BoxShadow(
+                  color: myConstants.primaryColor.withOpacity(.2),
+                  blurRadius: 7,
+                  offset: const Offset(0, 3),
+                ),
+              ],
+            ),
+            child: Row(
+              children: [
+                GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      cities[index].isSelected = !cities[index].isSelected;
+                    });
+                  },
+                  child: Image.asset(
+                    cities[index].isSelected
+                        ? 'assets/checked.png'
+                        : 'assets/unchecked.png',
+                    width: 30,
+                  ),
+                ),
+                const SizedBox(width: 10),
+                Text(
+                  cities[index].region,
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: cities[index].isSelected
+                        ? myConstants.primaryColor
+                        : Colors.black54,
+                  ),
+                )
+              ],
+            ),
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        // Add button here
+        backgroundColor: myConstants.secondaryColor,
+        child: const Icon(Icons.pin_drop),
+        onPressed: () {
+          print('Pressed');
+        },
+      ),
     );
   }
 }
